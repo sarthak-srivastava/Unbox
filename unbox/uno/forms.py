894df1f15138d0bc9e558431d28a32faf33b7967
Nikhil Ranjan <nikhilranjan7@gmail.com>
2018-03-17 15:16:01 +0530

    Add templates

--- unbox/uno/forms.py
+++ unbox/uno/forms.py
@@ -1,23 +1,60 @@
 from uno.models import Question_m
 from django import forms
 
+Budget = (
+    ('Less than 5000','Less than 5000'),
+    ('Less than 10000','Less than 10000'),
+    ('Less than 20000','Less than 20000'),
+    ('Less than 50000','Less than 50000'),
+    ('No constraint','No constraint'),
+)
+
+Profession = (
+    ('IT','IT'),
+    ('Engineer','Engineer'),
+    ('Doctor','Doctor'),
+    ('BusinessMan','BuisnessMan'),
+)
+
+Gender = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
+
+Age = (
+    ('10-20', '10-20'),
+    ('20-30', '20-30'),
+    ('30-40', '30-40'),
+    ('40+', '40+'),
+)
+
+Task = (
+    ('Battery Life', 'Battery Life'),
+    ('Camera', 'Camera'),
+)
+
 Location = (
-    ('goa','Goa'),
-    ('mumbai','Mumbai'),
-    ('bangalore','Bangalore'),
+    ('Urban','Urban'),
+    ('Rural','Rural'),
+)
+
+Assertion = (
+    ('Yes','Yes'),
+    ('No','No'),
 )
 
 class Question_f(forms.ModelForm):
-    location = forms.ChoiceField(choices=Location,
-                 help_text="Location of the Scrap generating Company")
-    name = forms.CharField(max_length=100,required=True,help_text="Name")
-    contact_info = forms.CharField(max_length=100,required=True,help_text="Contact Information")
-    item = forms.CharField(max_length=100,required=True,help_text="Item")
-    quantity = forms.CharField(max_length=100,required=True,help_text="Quantity")
-    price = forms.CharField(max_length=100,required=True,help_text="Price quoted")
-    additional_info = forms.CharField(max_length=1000,help_text="Additional Information")
-    certificates = forms.URLField(max_length=200, help_text="Google drive link to your authorized license", required=False)
+    budget = forms.ChoiceField(choices=Budget, help_text="Budget")
+    profession = forms.ChoiceField(choices=Profession, help_text="Profession")
+    gender = forms.ChoiceField(choices=Gender, help_text="Gender")
+    age = forms.ChoiceField(choices=Age, help_text="Age")
+    task = forms.ChoiceField(choices=Task, help_text="What is more important to you?")
+
+    location = forms.ChoiceField(choices=Location, help_text="Place of living")
+
+    prefer_to_chinese = forms.ChoiceField(choices=Assertion, help_text="Will you prefer Chinese brands for better specification")
+
 
     class Meta:
         model = Question_m
-        fields = ['name', 'contact_info', 'item', 'quantity', 'price', 'additional_info','location', 'certificates']
+        fields = ['budget', 'profession', 'gender', 'age', 'task', 'location','location', 'prefer_to_chinese']
--- unbox/uno/models.py
+++ unbox/uno/models.py
@@ -1,26 +1,62 @@
 from django.db import models
 import datetime
 
-Location = (
-    ('goa','Goa'),
-    ('mumbai','Mumbai'),
-    ('bangalore','Bangalore'),
+Budget = (
+    ('Less than 5000','Less than 5000'),
+    ('Less than 10000','Less than 10000'),
+    ('Less than 20000','Less than 20000'),
+    ('Less than 50000','Less than 50000'),
+    ('No constraint','No constraint'),
 )
 
+Profession = (
+    ('IT','IT'),
+    ('Engineer','Engineer'),
+    ('Doctor','Doctor'),
+    ('BusinessMan','BuisnessMan'),
+)
+
+Gender = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
+
+Age = (
+    ('10-20', '10-20'),
+    ('20-30', '20-30'),
+    ('30-40', '30-40'),
+    ('40+', '40+'),
+)
+
+Task = (
+    ('Battery Life', 'Battery Life'),
+    ('Camera', 'Camera'),
+)
+
+Location = (
+    ('Urban','Urban'),
+    ('Rural','Rural'),
+)
+
+Assertion = (
+    ('Yes','Yes'),
+    ('No','No'),
+)
+
+
 class Question_m(models.Model):
-  location = models.CharField(max_length=100, choices=Location,
-               default='goa')
-  name = models.CharField(max_length=100)
-  contact_info = models.CharField(max_length=100)
-  item = models.CharField(max_length=100)
-  quantity = models.CharField(max_length=100)
-  price = models.CharField(max_length=100)
-  additional_info = models.CharField(max_length=1000)
-  certificates = models.URLField(default='null')
+    budget = models.CharField(max_length=100, choices=Budget, default='No constraint')
+    profession = models.CharField(max_length=100, choices=Profession, default='IT')
+    gender = models.CharField(max_length=100, choices=Gender, default='Male')
+    age = models.CharField(max_length=100, choices=Age, default='20-30')
+    task = models.CharField(max_length=100, choices=Task, default='Camera')
 
+    location = models.CharField(max_length=100, choices=Location, default='Urban')
 
-  class Meta:
+    prefer_to_chinese = models.CharField(max_length=100, choices=Assertion, default='No')
+
+    class Meta:
       verbose_name_plural = 'Questions'
 
-  def __str__(self):
+    def __str__(self):
       return self.item
